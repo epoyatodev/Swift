94e572b7d985805bc3f4c22ebda2ae6c55451fd7
Enrique <enripoor@hotmail.es>
2022-12-11 11:15:43 +0100

    no message

---
+++ Ejercicio6.2.playground/Contents.swift
@@ -0,0 +1,11 @@
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
+let numbers = Array()
+let result = numbers.chunked(into: 5)
+print(result)
