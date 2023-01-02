88e047db489cba0385c3d2c0320a6a2984d2870b
Enrique <enripoor@hotmail.es>
2023-01-02 17:31:52 +0100

    Añado marcar como favoritos y refrescar vista arrastrando hacia abajo.

---
+++ Fundamentos-IOS/ProyectoDos/ProyectoDos/Models/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  DragonBall
+//
+//  Created by Enrique Poyato Ortiz on 27/12/22.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: String
+    let apell1: String
+    let name: String
+    let photo: String
+    let apell2: String
+    let email: String
+    
+}
